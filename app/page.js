756ffd392335b3e942e10a151ff4359dedf47513
Bionuir <darktwt@gmail.com>
2025-06-29 18:29:11 -0500

    2v

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
 import { redirect } from "next/navigation";
-import { auth } from "../lib/firebase";
+import { auth } from "./lib/firebase";
 
 export default function Home() {
   redirect("/main");
